0a6bcf6c38f8a1df9572ebca75356eff42ea4a37
Dricera <7955817+Dricera@users.noreply.github.com>
2022-09-09 12:10:32 +0530

    initial implementation for Ticket model

---
+++ backend/Models/TicketModel.cs
@@ -0,0 +1,14 @@
+namespace backend.Models
+{
+	public class TicketModel
+	{
+		private DateTimeOffset createdDate;
+
+		public Guid TicketId { get; set; }
+		public string Description { get; set; }
+		public DateTimeOffset CreatedDate { get => createdDate; set => createdDate = DateTimeOffset.UtcNow; }
+		public DateTimeOffset UpdatedDate { get; set; }
+		public DateTimeOffset CompletedDate { get; set; }
+
+	}
+}
