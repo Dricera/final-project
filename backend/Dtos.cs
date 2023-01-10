a4c033eaa146ba5b1d9076b6ed81d5cb5b6f1b64
Dricera <7955817+Dricera@users.noreply.github.com>
2023-01-10 18:23:35 +0530

    Remove User-handling logic for now to prioritize Ticket implementation

--- backend/Dtos.cs
+++ backend/Dtos.cs
@@ -3,9 +3,10 @@ using System.ComponentModel.DataAnnotations;
 namespace backend.Dtos
 {
 	// public record UserDto(Guid id, string name, DateTimeOffset CreatedDate);
-	public record UserDto(Guid Id, string name, string role, DateTimeOffset CreatedDate);
+
+	/* public record UserDto(Guid Id, string name, string role, DateTimeOffset CreatedDate);
     public record CreateUserDto([Required] string name, string role);
-    public record UpdateUserDto([Required] string name, string role );
+    public record UpdateUserDto([Required] string name, string role ); */
 
 	public record TicketDto(Guid TicketId, string Title, string Description, DateTimeOffset CreatedDate);
 
