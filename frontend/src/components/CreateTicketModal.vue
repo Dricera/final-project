4d88f80e0b83045b0c0ab7fba657a8f9974790e5
Areeb <reachareeb@gmail.com>
2023-01-27 19:25:05 +0530

    create ticket modal component modified with added fields

--- frontend/src/components/CreateTicketModal.vue
+++ frontend/src/components/CreateTicketModal.vue
@@ -4,25 +4,63 @@
       class="q-pa-md text-white-5"
       style="width: 500px; max-width: 80vw;"
     >
-      <q-card-section>
-        <div class="text-center q-pt-lg">
-          <div class="col text-h5 ellipsis">
-            Create Ticket
-          </div>
-          <q-btn
-            v-close-popup
-            class="absolute-top-right"
-            icon="close"
-            flat
-            round
-            dense
-          />
-        </div>
-      </q-card-section>
       <q-card-section horizontal>
         <q-card-section>
+          <div class="text-center q-pt-lg">
+            <div class="col text-h5 ellipsis">
+              Create Ticket
+            </div>
+          </div>
+          <q-form
+            class="q-gutter-md"
+            @submit="createTicket"
+          >
+            <q-input
+              v-model="Text_e_id"
+              label="Employee ID"
+              standout="bg-green text-white"
+              :rules="[ val => val && val.length > 0 || 'This field is required']"
+            />
 
+            <q-input
+              v-model="Text_e_name"
+              label="Employee Name"
+              standout="bg-green text-white"
+              :rules="[ val => val && val.length > 0 || 'This field is required']"
+            />
+
+            <q-input
+              v-model="Text_e_dep"
+              label="Department"
+              standout="bg-green text-white"
+              :rules="[ val => val && val.length > 0 || 'This field is required']"
+            />
+
+            <q-input
+              v-model="Text_des"
+              label="Designation"
+              standout="bg-green text-white"
+              :rules="[ val => val && val.length > 0 || 'This field is required']"
+            />
+
+            <q-select
+              v-model="Select_type"
+              filled
+              :options="type_options"
+              label="Type"
+            />
+
+            <q-select
+              v-model="Select_priority"
+              filled
+              :options="priority_options"
+              label="Priority"
+            />
+          </q-form>
         </q-card-section>
+
+        <q-separator vertical />
+
         <q-card-section>
           <q-form
             class="q-gutter-md"
@@ -42,7 +80,6 @@
               label="Ticket Description"
               :rules="[ val => val && val.length > 0 || 'This field is required']"
             />
-
             <div>
               <q-btn
                 class="q-px-md q-py-sm"
@@ -51,17 +88,53 @@
                 color="primary"
               />
             </div>
+            <div>
+              <q-btn
+                round
+                color=""
+                icon="shopping_cart"
+              />
+            </div>
           </q-form>
         </q-card-section>
       </q-card-section>
     </q-card>
   </div>
+  <q-form>
+    <q-card-section>
+      <q-btn
+        v-close-popup
+        class="absolute-top-right"
+        icon="close"
+        flat
+        round
+        dense
+      />
+    </q-card-section>
+  </q-form>
 </template>
 
 <script>
+import { ref } from 'vue'
 
 export default {
   emits: ['createDone'],
+  setup () {
+    return {
+      Text_e_id: ref(''),
+      Text_e_name: ref(''),
+      Text_dep: ref(''),
+      Text_des: ref(''),
+      Select_type: ref(null),
+      type_options: [
+        '1', '2', '3', '4'
+      ],
+      Select_priority: ref(null),
+      priority_options: [
+        '1', '2', '3,', '4'
+      ]
+    }
+  },
   data () {
     return {
     //
