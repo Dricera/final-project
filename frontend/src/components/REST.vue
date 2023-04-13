15c427c806672007b901652c0341ff662507bf56
Dricera <7955817+Dricera@users.noreply.github.com>
2023-04-13 07:08:59 +0000

    fix api call

--- frontend/src/components/REST.vue
+++ frontend/src/components/REST.vue
@@ -26,7 +26,7 @@ export default {
       await fetch(API_URL, fetchOptions)
     },
     async patchTicket (ticketID, ticketPatch) {
-      const API_URL = 'https://localhost:5001/api/ticket/' + ticketID
+      const API_URL = '/api/ticket/' + ticketID
       const fetchOptions = {
         method: 'PATCH',
         headers: {
